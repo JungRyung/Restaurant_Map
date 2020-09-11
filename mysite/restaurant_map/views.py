5369a75f66c8cf4907a8258c3ce047dff249f4b8
JungRyung <37282346+JungRyung@users.noreply.github.com>
2020-09-11 13:09:31 +0900

    update

--- mysite/restaurant_map/views.py
+++ mysite/restaurant_map/views.py
@@ -1,16 +1,13 @@
 from django.views.generic.base import TemplateView
 from django.views.generic import ListView
 from django.views.generic import DetailView
-from restaurant_map.models import RestaurantList
+from restaurant_map.models import Restaurant, Comment
 
 class IndexView(TemplateView):
     template_name = 'restaurant_map/index.html'
 
-class MapView(TemplateView):
-    template_name = 'restaurant_map/map.html'
-    res_list = RestaurantList.objects
-    context = {'res_list':res_list}
-    def get_context_data(self):
-        context = super().get_context_data()
-        context['name'] = '공유'
-        return context
+class MapView(ListView):
+    model = Restaurant
+
+class CommentList(ListView):
+    model = Comment
