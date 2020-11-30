924eccd5c182b985fe1a5be39a11755e0ec0d93d
JungRyung <37282346+JungRyung@users.noreply.github.com>
2020-11-30 09:29:59 +0900

    update restaurant app

--- mysite/restaurant_map/urls.py
+++ mysite/restaurant_map/urls.py
@@ -7,5 +7,5 @@ app_name = 'restaurant_map'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('map/', views.MapView.as_view(), name='map'),
-    path('comment/name<str:name>/', views.CommentList.as_view(), name='comment'),
+    path('comment/<str:name>/', views.CommentList.as_view(), name='comment'),
 ]
