01899358d872d41834923dc75635f61cdf448aff
Jung Ryung <ryung@Jungui-MacBookAir.local>
2021-03-08 01:41:19 +0900

    update logout

--- mysite/restaurant_map/views.py
+++ mysite/restaurant_map/views.py
@@ -3,12 +3,14 @@ from django.views.generic import ListView
 from django.views.generic import DetailView
 from restaurant_map.models import Restaurant, Comment
 from django.views import View
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 class IndexView(TemplateView):
     template_name = 'restaurant_map/index.html'
 
-class MapView(ListView):
+class MapView(LoginRequiredMixin,ListView):
     model = Restaurant
+    login_url = '/common/login/'
 
 class RestaurantDetail(DetailView):
     model = Restaurant
