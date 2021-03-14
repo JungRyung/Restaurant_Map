f27f068acf60760f3ebf9d28c23cbf714acb4b53
Jung Ryung <ryung@Jungui-MacBookAir.local>
2021-03-14 21:19:00 +0900

    modify model

--- mysite/restaurant_map/models.py
+++ mysite/restaurant_map/models.py
@@ -6,17 +6,19 @@ from django.db import models
 # python manage.py migrate
 
 class Restaurant(models.Model):
-	name = models.CharField(max_length=50)
+	id = models.BigAutoField(help_text="Restaurant ID", primary_key=True)
+	name = models.CharField(max_length=50,blank=False,null=False)
 	address = models.CharField(max_length=100)
 	def __str__(self):
 		return self.name
 
 class Comment(models.Model):
-	username = models.CharField(max_length=50, default = '')
-	restaurant = models.ForeignKey('Restaurant', on_delete=models.CASCADE)
-	visiting_day = models.DateField()
-	comment = models.CharField(max_length=500)
-	grade = models.IntegerField()
+	id = models.BigAutoField(help_text="Comment ID", primary_key=True)
+	restaurant_id = models.ForeignKey("Restaurant", related_name="restaurant", on_delete=models.CASCADE, db_column="restaurant_id")
+	username = models.CharField(max_length=50,blank=False,null=False)
+	visiting_day = models.DateField(blank=False,null=False)
+	comment = models.CharField(max_length=500,blank=False,null=False)
+	grade = models.IntegerField(blank=False,null=False)
 
 	def __str__(self):
 		return self.username
